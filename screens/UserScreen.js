f058efff95a2f90587c7231e068a6df002ffbd21
tuonghuynh11 <89913828+tuonghuynh11@users.noreply.github.com>
2023-08-22 20:58:15 +0700

    b

---
+++ screens/UserScreen.js
@@ -0,0 +1,31 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+function UserScreen({ route, navigation }) {
+  function openDrawerHandler() {
+    navigation.toggleDrawer();
+  }
+  return (
+    <View style={styles.rootContainer}>
+      <Text>
+        This is the <Text style={styles.highlight}>"User"</Text> screen!
+      </Text>
+      <Button title="Open Drawer" onPress={openDrawerHandler}></Button>
+    </View>
+  );
+}
+
+export default UserScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  highlight: {
+    fontWeight: "bold",
+    color: "#eb1064",
+  },
+});
