ee1700289cc4f151d7f42d99d33945ff61b108c0
tuonghuynh11 <89913828+tuonghuynh11@users.noreply.github.com>
2023-08-22 20:51:52 +0700

    a

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+('react-native/Libraries/NewAppScreen');
+
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
